13f40dd0911618447376ae5c62e6289641cb7e5d
Yary <yarisebas9962@gmail.com>
2021-11-08 17:33:10 -0500

    main

--- index.php
+++ index.php
@@ -1,19 +1,23 @@
 <?php
 
 
-$register= array(
+require_once __DIR__ . '/vender/autoload.php';
 
-    
-        
-    "name"=>"miguel",
-    "edad"=>"miguel",
-    "register"=>"15",
+$client = new MongoDB\Client(
+    'mongodb+srv://sebastian:<password>@cluster0.kwxgh.mongodb.net/sebastian?retryWrites=true&w=majority');
 
-
-);
-
-
-echo json_encode($register);
-
+    $tb=$client->water->Precipitaciones;
+$filter= ['$and'=>
+            [
+                ['ANNO'=>['$eq'=>'1997']],
+                ['ESTACION'=>['$eq'=>'Sutatausa']]
+            ]
+        ];
+ 
+$query = new MongoDB\Driver\Query($filter);
+ 
+$rows = $tb->find($filter);
+$datos= iterator_to_array($rows);
+echo json_encode($datos);
 
 ?>
