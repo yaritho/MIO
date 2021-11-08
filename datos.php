81aa9f74269affd6bf5e8955438c6ed3df8041be
Yary <yarisebas9962@gmail.com>
2021-11-08 17:13:37 -0500

    main

--- datos.php
+++ datos.php
@@ -4,19 +4,19 @@ require_once __DIR__ . '/vender/autoload.php';
 $client = new MongoDB\Client(
     'mongodb+srv://sebastian:<password>@cluster0.kwxgh.mongodb.net/sebastian?retryWrites=true&w=majority');
 
-    $tb=$client->sebastian->user;
-    $register= array(
-
-    
-        
-        "user"=>"aldana",
-        "pass"=>"abc123",
-
-    );
-
-    $resultado=$tb->insertOne($register);
-    echo json_encode($register);
-
+    $tb=$client->water->Precipitaciones;
+$filter= ['$and'=>
+            [
+                ['ANNO'=>['$eq'=>'1997']],
+                ['ESTACION'=>['$eq'=>'Sutatausa']]
+            ]
+        ];
+ 
+$query = new MongoDB\Driver\Query($filter);
+ 
+$rows = $tb->find($filter);
+$datos= iterator_to_array($rows);
+echo json_encode($datos);
 
 ?>
 
