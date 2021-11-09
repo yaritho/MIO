1bc90f1c5fa17bc63842a8eb27b7e2a7016dc4a9
Yary <yarisebas9962@gmail.com>
2021-11-08 19:24:03 -0500

    main

--- datos.php
+++ datos.php
@@ -2,10 +2,10 @@
 require_once __DIR__ . '/vender/autoload.php';
 
 $client = new MongoDB\Client(
-   // 'mongodb+srv://sebastian:yaritho123@cluster0.kwxgh.mongodb.net/sebastian?retryWrites=true&w=majority');
-   'mongodb+srv://sebastian:<password>@cluster0.kwxgh.mongodb.net/test');
+   'mongodb+srv://sebastian:yaritho123@cluster0.kwxgh.mongodb.net/sebastian?retryWrites=true&w=majority');
+   
 
-    $tb=$client->lluvia->Precipitacion;
+    $tb=$client->lluvia->precipitacion;
 $filter= ['$and'=>
             [
                 ['AÑO'=>['$eq'=>'1997']],
