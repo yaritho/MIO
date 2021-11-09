3a266379419291742c9db1cc6426e4f6a0354a89
Yary <yarisebas9962@gmail.com>
2021-11-08 19:27:18 -0500

    main

--- datos.php
+++ datos.php
@@ -2,8 +2,8 @@
 require_once __DIR__ . '/vender/autoload.php';
 
 $client = new MongoDB\Client(
-   'mongodb+srv://sebastian:yaritho123@cluster0.kwxgh.mongodb.net/sebastian?retryWrites=true&w=majority');
-   
+  // 'mongodb+srv://sebastian:yaritho123@cluster0.kwxgh.mongodb.net/sebastian?retryWrites=true&w=majority');
+   'mongodb+srv://sebastian:<password>@cluster0.kwxgh.mongodb.net/test');
 
     $tb=$client->lluvia->precipitacion;
 $filter= ['$and'=>
