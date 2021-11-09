d6d162a2469ebc1c76fda36d0d18699b2a74176c
Yary <yarisebas9962@gmail.com>
2021-11-08 19:07:02 -0500

    main

--- index.php
+++ index.php
@@ -1,4 +1,21 @@
 <?php
+require_once __DIR__ . '/vender/autoload.php';
 
-echo "hola mundo";
+$client = new MongoDB\Client(
+    'mongodb+srv://sebastian:Yaritho.g.e123@cluster0.kwxgh.mongodb.net/sebastian?retryWrites=true&w=majority');
+
+    $tb=$client->lluvia->Precipitacion;
+$filter= ['$and'=>
+            [
+                ['AÑO'=>['$eq'=>'1997']],
+                ['ESTACIÓN'=>['$eq'=>'Pajas Blancas']]
+            ]
+        ];
+ 
+$query = new MongoDB\Driver\Query($filter);
+ 
+$rows = $tb->find($filter);
+$datos= iterator_to_array($rows);
+echo json_encode($datos);
+
 ?>
